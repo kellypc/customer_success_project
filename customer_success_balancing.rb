f2c0743f45b3c8c4eb71b2bee5e7dd7fe86015dc
Kelly Cruz <kellycruz.ti@gmail.com>
2023-06-02 10:44:09 -0300

    Distribui customers por customer success

--- customer_success_balancing.rb
+++ customer_success_balancing.rb
@@ -1,29 +1,48 @@
-require 'byebug'
-
 class CustomerSuccessBalancing
-  attr_accessor :customer_success, :customers, :away_customer_success
+  attr_accessor :customer_success, :customers, :away_customer_success, :available_customer_success
 
   def initialize(customer_success, customers, away_customer_success)
     @customer_success = customer_success
     @customers = customers
     @away_customer_success = away_customer_success
+    @available_customer_success = []
   end
 
   def execute
-    customers_list
+    remove_away_customer_success
+    customers_list = balance_customer_by_customer_success
+    return customer_success_with_more_customers(customers_list)
   end
 
   private
+  
+  def balance_customer_by_customer_success
+    @customers.each do |customer|
+      @available_customer_success.each do |customer_success|
+        next if customer[:score] > customer_success[:score]
+        customer[:customer_success] = customer_success
+      end
+    end    
+    @customers
+  end
 
-  def customers_list
-    customers_list = []
-    @customer_success.map do |customer_success|
-      # Lista de todos os clientes pelo score do customer success 
-      customers = @customers.select{|x| x[:score] <= customer_success[:score] }
-      # Gera a lista com todos os customer_success e a quantidade de clientes
-      customers_list << {id: customer_success[:id], customers: customers.count}
+  def customer_success_with_more_customers(customers_list)    
+    @available_customer_success.each do |customer_success|
+      customer_success[:customers_size] = 0
+      customers_list.each do |customer|
+        if customer_success[:id] == customer[:customer_success][:id]
+          customer_success[:customers_size] += 1
+        end
+      end
     end
-    # Ordena pela quantidade de clientes e exibe o primeiro item da lista
-    customers_list.sort_by!{ |k| k["customers"]}.first[:id]
+    @customer_success.sort_by!{ |k| k["customers"]}.first[:id]
+  end
+
+  def remove_away_customer_success
+    @customer_success.each do |cs|
+      unless away_customer_success.include?(cs[:id])
+        @available_customer_success << cs
+      end
+    end
   end
 end
